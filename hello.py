4e30ed73a3d53dfb500a3733beb9a0115fa30c2b
Chitsanupat Wayusuwanwit <140317465+Doonminus2@users.noreply.github.com>
2025-08-06 11:19:52 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import config
 import datetime
 def say_hello(name):
     now = datetime.datetime.now()
-    print(f"Hello, {name} from {config.APP_NAME}!")
+    print(f"Goodbye, {name} from {config.APP_NAME}!")
     print(f"Today is {now.strftime('%Y-%m-%d')}.")
     
 def greet_user():
