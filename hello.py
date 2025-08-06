d47126817196704a5b30d4351d6de82cb889074d
Chitsanupat Wayusuwanwit <140317465+Doonminus2@users.noreply.github.com>
2025-08-06 11:11:34 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,21 +1,14 @@
 import config 
-<<<<<<< HEAD
-
-
-def say_hello(name):
-    print(f"Hello, {name} from {config.APP_NAME}!")
-=======
 import datetime
-
 def say_hello(name):
     now = datetime.datetime.now()
     print(f"Hello, {name} from {config.APP_NAME}!")
     print(f"Today is {now.strftime('%Y-%m-%d')}.")
-
->>>>>>> feature-date
+    
 def greet_user():
     name = input("please enter name: ")
     say_hello(name)
 
 if __name__ == "__main__":
     greet_user()
+
